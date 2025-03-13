c26661d7ef2545446121778b52e6034901bd35ca
Parthik10 <156055046+Parthik10@users.noreply.github.com>
2025-03-13 13:12:59 +0530

    small change

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -5,6 +5,6 @@ const router = express.Router();
 
 router.post("/register", register);
 router.post("/login", login);
-router.get("/register", getClubs); // Add route to get club details
+router.get("/getclubs", getClubs); // Add route to get club details
 
 module.exports = router;
